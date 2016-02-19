a7f534ed69d070f224b2ca392ee3a33bcb5dc5c5
Lavonen <anssi.lavonen@metropolia.fi>
2016-02-19 10:05:16 +0200

    cookieeees

---
+++ cookie.php
@@ -0,0 +1,29 @@
+<?php
+	
+	setcookie("nimi", "samu-esa");
+	setcookie("auto", "Honda", time() + 60 * 10);
+	
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+	<script>
+    	alert("Sivusto käyttää evästeitä, Jatkamalla hyväksyt ehdot! HÄHÄÄÄÄÄ");
+    </script>
+    <img src="cookiemonster.jpeg" /><br>
+    <?php
+		$nimi = $_COOKIE["nimi"];
+		$auto = $_COOKIE["auto"];
+		
+		echo "{$nimi}n auto on $auto";
+		
+	?>
+
+</body>
+</html>
